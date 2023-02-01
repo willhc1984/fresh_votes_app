d700f6fa24a6c401c01ae63c4d12741f5b31e5a4
will henrique <willhc1984@gmail.com>
2023-02-01 15:12:42 -0300

    Reply para comemntarios com JQuery

--- src/main/java/com/freshvotes/controller/CommentsController.java
+++ src/main/java/com/freshvotes/controller/CommentsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.freshvotes.model.Comment;
@@ -37,9 +38,21 @@ public class CommentsController {
 	}
 	
 	@PostMapping("")
-	public String postComment(@AuthenticationPrincipal User user, Comment comment, 
-			@PathVariable Long featureId, @PathVariable Long productId) {
+	public String postComment(@AuthenticationPrincipal User user, 
+			@PathVariable Long featureId, @PathVariable Long productId, Comment comment,
+			@RequestParam(required=false) Long parentId, @RequestParam(required=false) String childCommentText) {
+		
 		Optional<Feature> optionalFeature = featureRepository.findById(featureId);
+		
+		if(parentId != null) {
+			comment = new Comment();
+			Optional<Comment> optionalParentComment = commentRepository.findById(parentId);
+			
+			if(optionalParentComment.isPresent()) {
+				comment.setComment(optionalParentComment.get());
+				comment.setText(childCommentText);
+			}
+		}
 		
 		if(optionalFeature.isPresent()) {
 			comment.setFeature(optionalFeature.get());
